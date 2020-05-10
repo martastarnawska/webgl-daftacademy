06b3313330452cbed3b55d711d3f758e68da23a2
Marta <marta.chelm@gmail.com>
2020-05-10 23:09:55 +0200

    prepare to heroku deploy

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(8000, () => {
-  console.log('webgl-daftacademy on 8000!')
+app.listen(process.env.PORT || 3000, () => {
+  console.log('webgl-daftacademy on 3000!')
 });
